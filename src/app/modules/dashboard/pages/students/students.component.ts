edf7015b37e384a6c30e27d3d82211eaa65163ea
Diego <rojas_diego@outlook.com>
2025-01-25 16:16:39 -0300

    UPD student component

--- src/app/modules/dashboard/pages/students/students.component.ts
+++ src/app/modules/dashboard/pages/students/students.component.ts
@@ -6,7 +6,7 @@ import { generateRandomString } from '../../../../shared/utils';
 @Component({
   selector: 'app-students',
   standalone: false,
-  
+
   templateUrl: './students.component.html',
   styleUrl: './students.component.scss'
 })
@@ -22,6 +22,8 @@ export class StudentsComponent {
     }
   ];
 
+  editingStudentId: string | null = null;
+
 
   constructor(private fb: FormBuilder) {
     this.studentForm = this.fb.group({
@@ -35,24 +37,53 @@ export class StudentsComponent {
       this.studentForm.markAllAsTouched();
     } else {
 
-      this.students = [
-        ...this.students,
-        {
+      if (!!this.editingStudentId) {
+        //editar
+        this.students = this.students.map((student) =>
+          student.id === this.editingStudentId
+            ? { ...student, ...this.studentForm.value }
+            : student);
+
+        this.editingStudentId = null;
+      } else {
+        //crear
+        this.students = [
+          ...this.students,
+          {
+            id: generateRandomString(6),
+            ...this.studentForm.value
+          }
+        ];
+        /*this.students.push({
           id: generateRandomString(6),
           ...this.studentForm.value
-        }
-      ];
-      /*this.students.push({
-        id: generateRandomString(6),
-        ...this.studentForm.value
-      }); */
+        }); */
+      }
+
     }
+
+    this.studentForm.reset();
+
   };
 
   onDelete(id: string) {
     if (confirm('Esta seguro que desea eliminar el registro?')) {
       this.students = this.students.filter(student => student.id !== id);
     }
+  };
+
+  onEdit(student: Student): void {
+    console.log('Se va a editar el estudiante', student);
+
+    this.editingStudentId = student.id;
+
+    this.studentForm.patchValue({
+      name: student.name,
+      lastName: student.lastName
+    });
+
+
+
   };
 
 }
